26dbc2a3cb420adcad2eef7d659f3f3693997178
irvingHerrera <irving.herreramolina@gmail.com>
2018-01-05 16:32:57 -0600

    resource

---
+++ App3/App3/Helper/Languages.cs
@@ -0,0 +1,39 @@
+﻿using App3.Interfaces;
+using App3.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App3.Helper
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Tile
+        {
+            get { return Resource.Tile; }
+        }
+
+        public static string Accept
+        {
+            get { return Resource.Accept; }
+        }
+
+        public static string AmountValidation
+        {
+            get { return Resource.AmountValidation; }
+        }
+
+        public static string Error
+        {
+            get { return Resource.Error; }
+        }
+    }
+}
---
+++ App3/App3/Helper/PlatformCulture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.Helper
+{
+    public class PlatformCulture
+    {
+        public PlatformCulture(string platformCultureString)
+        {
+            if (string.IsNullOrEmpty(platformCultureString))
+            {
+                throw new ArgumentException("Expected culture identifier", nameof(platformCultureString));
+            }
+
+            PlatformString = platformCultureString.Replace("_", "-");
+
+            var dashIndex = platformCultureString.IndexOf("", StringComparison.Ordinal);
+
+            if (dashIndex > 0)
+            {
+                var parts = PlatformString.Split('-');
+            }
+            else
+            {
+                LanguageCode = PlatformString;
+                LocaleCode = string.Empty;
+            }
+        }
+
+
+        public string PlatformString { get; set; }
+        public string LanguageCode { get; set; }
+        public string LocaleCode { get; set; }
+
+        public override string ToString()
+        {
+            return PlatformString;
+        }
+    }
+}
