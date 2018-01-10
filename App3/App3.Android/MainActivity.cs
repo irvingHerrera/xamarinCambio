5c47290b0a9a803e4b782a42e757ca53774048dc
irvingHerrera <irving.herreramolina@gmail.com>
2018-01-10 12:40:40 -0600

    add icon

--- App3/App3.Android/MainActivity.cs
+++ App3/App3.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.OS;
 namespace App3.Droid
 {
     
-    [Activity(Label = "App3", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "App3", Icon = "@drawable/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
